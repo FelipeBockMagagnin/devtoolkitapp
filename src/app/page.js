b9b3f05286bee09fcc34312070c0a3ea49e54848
felpi <felipebmagagnin#gmail.com>
2023-06-29 11:27:28 -0300

    feat: add google analytics

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
 import React from "react"
 import Title from "@/components/Title"
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <div>
+
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-SFFSZM7F21" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+      </Script>
       <Title><div className="text-center">Dev Toolkit</div></Title>
 
       <div className="mb-2 text-center text-gray-500 dark:text-gray-400">
