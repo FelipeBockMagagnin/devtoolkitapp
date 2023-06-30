60ce6d143c4860ec8bfcc7a23466b8009e0ced93
felpi <felipebmagagnin#gmail.com>
2023-06-29 21:28:18 -0300

    feat: add menu listing in home screen

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,29 +2,12 @@
 import React, { useEffect } from "react";
 import { LuFileJson } from 'react-icons/lu';
 import { initFlowbite } from 'flowbite'
+import menus from '../data/menus'
 
 export default function SideBar() {
     useEffect(() => {
         initFlowbite()
     })
-
-    let menus = [
-        {
-            name: 'JSON Minifier',
-            icon: LuFileJson,
-            link: '/minify-json'
-        },
-        {
-            name: 'JSON Formatter',
-            icon: LuFileJson,
-            link: '/format-json'
-        },
-        {
-            name: 'Lorem Ipsum',
-            icon: LuFileJson,
-            link: '/lorem-ipsum'
-        }
-    ]
 
     return (
         <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
---
+++ src/data/menus.js
@@ -0,0 +1,22 @@
+import { LuFileJson } from 'react-icons/lu';
+
+const menus = [
+    {
+        name: 'JSON Minifier',
+        icon: LuFileJson,
+        link: '/minify-json'
+    },
+    {
+        name: 'JSON Formatter',
+        icon: LuFileJson,
+        link: '/format-json'
+    },
+    {
+        name: 'Lorem Ipsum Generator',
+        icon: LuFileJson,
+        link: '/lorem-ipsum'
+    }
+]
+
+
+export default menus;
