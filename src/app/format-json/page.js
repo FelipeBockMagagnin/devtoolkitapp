055950f3913ac0bf1e411782ae2afece5d4a7424
Felipe Magagnin <felipe.magagnin@multi24h.com.br>
2023-10-03 16:51:08 -0300

    chore: add metadata to json formatter

--- src/app/format-json/page.js
+++ src/app/format-json/page.js
@@ -1,52 +1,10 @@
-'use client'
+import FormatJson from "@/components/format-json";
 
-import Title from "@/components/Title";
-import { useState } from "react";
+export const metadata = {
+    title: 'Json Formatter - Dev Toolkit',
+    description: 'Format and beautify your json in one click, maximize website performance with this efficient JSON formatter online free tool.',
+}
 
 export default function Page() {
-    const [jsonOutput, setJsonOutput] = useState('');
-    const [jsonError, setJsonError ] = useState('')
-
-    function format_json(e) {
-        setJsonError('')
-        setJsonOutput('')
-
-        if(!e.target.value){
-            return
-        }
-
-        let json = e.target.value;
-        try{
-            var jsonObject = JSON.parse(json);
-        } catch(err) {
-            console.log(err.message);
-            setJsonError(err.message)
-            return
-        }
-       
-
-        var newString = JSON.stringify(jsonObject, null, '\t')
-
-        setJsonOutput(newString)
-    }
-
-    return (
-        <>
-            <Title>JSON Formatter</Title>
-
-            <div  className="grid gap-6 mb-6 md:grid-cols-2">
-                <div>
-                    <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Input</label>
-                    <textarea onChange={(event) => format_json(event)} id="message" rows="6" className="sm:h-96 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" ></textarea>
-                </div>
-
-                <div>
-                    <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Output</label>
-                    <textarea disabled value={jsonOutput} id="message" rows="6" className="sm:h-96 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" ></textarea>
-                    <p className="text-xs text-red-600 dark:text-white">{jsonError}</p>
-                </div>
-            </div>
-
-        </>
-    )
+    return <FormatJson/>
 }
