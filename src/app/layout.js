6eb419546cc07241c1f5fea10550f5fc2cbe1308
Felipe Magagnin <felipebmagagnin@gmail.com>
2024-01-22 09:15:43 -0300

    chore: add adsense

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { Flowbite } from '../lib/flowbite';
 import SideBar from '@/components/SideBar';
 import NavBar from '@/components/NavBar';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
    return (
       <html lang="en">
+         <head>
+            { process.env.NODE_ENV == "production" && <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8872911814581203" crossOrigin="anonymous"></Script> }
+         </head>
          <body className={[inter.className + '; dark:bg-gray-800']}>
             <Flowbite>
                <NavBar></NavBar>
