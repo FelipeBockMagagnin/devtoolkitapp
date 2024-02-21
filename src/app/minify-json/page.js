baa3dfdaf451def5c8745791fc36b82335cea33b
Felipe Magagnin <felipe.magagnin@multi24h.com.br>
2024-02-21 17:22:35 -0300

    chore: improve json minifier seo meta tag

--- src/app/minify-json/page.js
+++ src/app/minify-json/page.js
@@ -2,7 +2,7 @@ import MinifyJson from '../../components/minify-json';
 
 export const metadata = {
     title: 'JSON Minifier - Dev Toolkit',
-    description: 'Minify and compact your JSON (JavaScript Object Notation), removing unnecessary characters, such as whitespace and comments with this online free tool.',
+    description: 'Minify and compact your JSON (JavaScript Object Notation), removing unnecessary characters, such as whitespace and comments with this JSON minifier online tool.',
 }
 
 export default function Page() {
